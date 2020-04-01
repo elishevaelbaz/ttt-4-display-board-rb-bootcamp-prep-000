f481949f20cf46fc0a14327cb30cecea83d785d6
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-01 13:55:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 
 def display_board(board)
-  puts " #{board[0]} | #{array[1]} | #{array[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts"-----------"
-  puts " #{array[3]} | #{array[4]} | #{array[5]}  "
+  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
   puts"-----------"
-  puts " #{array[6]} | #{array[7]} |  #{array[8]} "
+  puts " #{board[6]} | #{board[7]} |  #{board[8]} "
 end
